85d2d1fe5dbad26daa3142e9ae1bf98afce1a64e
naianesantana <nextgoriginal@gmail.com>
2026-02-22 09:16:57 -0300

    :ok_hand: feat: finaliza processamento de dados e formatação do relatório Excel

--- processamento_vale.py
+++ processamento_vale.py
@@ -1,51 +1,103 @@
 import pandas as pd
+pd.set_option('display.max_columns',None)
 
-# --- REGRAS DE NEGÓCIO ---
-def calcular_valor_vale(horas):
-    '''
-    a função recebe uma variável genérica,
-    valida com o valor da coluna Trabalhado
-    retorna a validação à coluna Valor VA
-    '''
-    if horas >= 7: #
-        return  27.00 #
-    else:
+#Variáveis que recebem os arquivos .csv
+arquivo_ponto = 'exportacao-relatorio-ponto-diario.csv'
+arquivo_abono = 'exportacao-ausencias.csv'
+
+#Funções para conversão dos tipos de dados
+def converter_data(df,coluna,formato):
+    df[coluna] = df[coluna].str.strip()
+    df[coluna] = pd.to_datetime(df[coluna],format=formato)
+    return df
+
+def converter_tempo(df,coluna):
+    df[coluna] = (df[coluna]
+                  .fillna('00:00')
+                  .str.strip())
+    df[coluna] = pd.to_timedelta(df[coluna] + ':00')
+    df[coluna] = df[coluna].dt.total_seconds() / 3600
+    return df
+
+#Função com as regras de negócio para sucesso no cálculo do valor do vale alimentação diário
+def calcular_valor_vale(linha):
+    carga_horaria = linha['Carga horária']
+    trabalhado = linha['Trabalhado']
+    dia_semana = linha['Dia semana']
+
+    if trabalhado == 0:
+        return 0.00
+    elif carga_horaria < 8 and 0 < trabalhado < 8 and dia_semana != 'sáb' and dia_semana != 'dom':
         return 10.00
+    else:
+        return 27.00
 
-# --- PROCESSO PRINCIPAL ---
+#1. Leitura das variáveis e especificação de quais colunas serão consideradas
+df_ponto = pd.read_csv(arquivo_ponto,sep=';',
+                 usecols=['Data','Funcionário','Trabalhado','Carga horária'])
 
-#1. Importação do arquivo --- Leitura do arquivo .csv e especificação do separador ;
-df = pd.read_csv('ponto-diario-JAN2026.csv',sep=';')
+df_abono = pd.read_csv(arquivo_abono,sep=';',
+            usecols=['Tipo','Funcionário','Ocorrência','Data inicial'])
 
+#2. Início do processo de Tratamento dos dados
+df_ponto['Data'] = df_ponto['Data'].ffill()
 
-#2. Tratamento do arquivo
-df.drop(columns=['Registros','Extras','Faltas'],inplace=True) #Remove as colunas Registros, Extras e Faltas
-df['Data'] = df['Data'].ffill() #Preenche datas vazias replicando a anterior
+df_ponto['Dia semana'] = (df_ponto['Data']
+                    .str.split()
+                    .str.get(-1)
+                    .str.strip())
 
-#Remove linhas onde o Funcionário ou Carga horária possuem valor vazios (NaN)
-df.dropna(subset=['Funcionário','Carga horária'], inplace=True)
+df_ponto['Data'] = (df_ponto['Data']
+              .str.split()
+              .str.get(0)
+              .str.strip())
 
-df['Trabalhado'] = df['Trabalhado'].str.strip() #Limpa os espaços em branco
-df = df[df['Trabalhado'] != '00:00'] #Remove dias não trabalhados
+df_ponto.dropna(subset='Funcionário', inplace=True)
 
-#Conversão de tempo: Transforma HH:MM em duração e depois em número decimal para cálculos
-df['Trabalhado'] = pd.to_timedelta(df['Trabalhado'] + ':00')
-df['Trabalhado'] = df['Trabalhado'].dt.total_seconds() / 3600
+df_abono['Ocorrência'] = (df_abono['Ocorrência']
+                          .str.split()
+                          .str.get(-1)
+                          .str.strip('()'))
 
+#Chamada das funções de conversão dos dados para datetime
+df_ponto= converter_data(df_ponto,'Data','%d/%m/%y')
+df_abono = converter_data(df_abono,'Data inicial','%d/%m/%Y')
 
-#3. Cálculos - Aplica a função de regra de negócio linha por linha
-df['Valor VA'] = df['Trabalhado'].apply(calcular_valor_vale)
+#Filtro que permiti somente as linhas com tipo Abono
+df_abono = df_abono[df_abono['Tipo'].str.contains('Abono',na=False)]
 
-#Soma o valor completo de Vale-alimentação por funcionário
-resultado = df.groupby('Funcionário')['Valor VA'].sum().reset_index() #.reset_index() transforma em uma tabela novamente
+#3. Agrupamento dos dois dataframes
+df_agrupado = pd.merge(
+    df_ponto,
+    df_abono,
+    how='left',
+    left_on=['Funcionário','Data'],
+    right_on=['Funcionário','Data inicial']
+)
 
+#4. Continuação no Tratamento dos dados (Agora dos dataframes agrupados)
 
-#4. Exportação para Excel
-nome_arquivo = 'Relatorio-Vale-Alimentacao.xlsx'
+#Chamada das funções de coversão para tempo e númerico
+df_agrupado = converter_tempo(df_agrupado,'Carga horária')
+df_agrupado = converter_tempo(df_agrupado,'Trabalhado')
+df_agrupado = converter_tempo(df_agrupado,'Ocorrência')
 
-#Gera o arquivo com duas abas: uma com detalhamento diário e outro com o valor mensal do VA
+df_agrupado['Tipo'] = df_agrupado['Tipo'].fillna('-')
+df_agrupado['Trabalhado'] = df_agrupado['Trabalhado'] + df_agrupado['Ocorrência']
+
+#5. Crição de um df final que receberá o valor diário do VA
+df_final = df_agrupado[['Data','Dia semana', 'Funcionário','Trabalhado','Carga horária']]
+
+df_final['Valor VA'] = df_final[['Trabalhado','Carga horária','Dia semana']].apply(calcular_valor_vale, axis=1)
+df_final['Data'] = df_final['Data'].dt.strftime('%d/%m/%Y')
+
+#6. Emissão do relatório em Excel com os valores diários e mensal - agrupado por funcionário
+resultado = df_final.groupby('Funcionário')['Valor VA'].sum().reset_index()
+
+nome_arquivo = 'Relatorio-VA.xlsx'
+
 with pd.ExcelWriter(nome_arquivo) as writer:
-    df.to_excel(writer,sheet_name='Relatorio Diário',index=False)
+    df_final.to_excel(writer,sheet_name='Relatorio Diário',index=False)
     resultado.to_excel(writer,sheet_name='Valor Mensal',index=False)
 
-
+    print('Relatório salvo!')
